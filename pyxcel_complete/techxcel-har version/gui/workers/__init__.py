f1238ee6afd51bf08e0e76d329491ec4afb386d7
KavinilaLoganathan-2507 <kavinilaloganathan@gmail.com>
2026-03-17 09:24:40 +0530

    Delete pyxcel_complete/techxcel-har version directory

--- pyxcel_complete/techxcel-har version/gui/workers/__init__.py
+++
@@ -1,6 +0,0 @@
-from gui.workers.agent_worker import (
-    MacroWorker, FormulaWorker, CleanerWorker,
-    ChatWorker, KpiWorker, InspectorWorker,
-    PivotWorker, ChartWorker, ChartPreviewWorker,
-    PdfWorker, MergerWorker,
-)
